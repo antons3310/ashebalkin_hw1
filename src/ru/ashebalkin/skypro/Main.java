1baf50e52b2f81c784806f8a30283024bfcde94b
antoshka <ashebalkin@diasoft.ru>
2021-10-11 23:38:10 +0300

    Мой второй коммит - ashebalkin

--- src/ru/ashebalkin/skypro/Main.java
+++ src/ru/ashebalkin/skypro/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Мой второй комит скайпро");
+        System.out.println("Мой второй комит скайпро - ashebalkin");
     }
 }
